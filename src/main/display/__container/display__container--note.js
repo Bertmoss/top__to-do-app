a953aa35e461a25860e658a783941c66d1dbab86
Bertmoss <b3rtm0ss@gmail.com>
2022-10-28 13:05:40 +0200

    Add edit to note display

--- src/main/display/__container/display__container--note.js
+++ src/main/display/__container/display__container--note.js
@@ -1,11 +1,15 @@
 import {createTable} from "../../../general/general__js/_table"
+import {createBasicInput, createTextArea} from "../../../general/general__js/_input"
+import {displayMod} from "../../main-pub-sub"
+
 
 let noteDisplay = document.createElement("div");
 noteDisplay.classList.add("display__container-note--hidden","display__container-note");
 
 function subscribeNote(obj) {
-  /* delete btn */
+  /*DELETE BTN*/
   let noteDiv = document.createElement("div");
+  noteDiv.setAttribute("data-id", obj.id);
   let dltBtn = document.createElement("button");
   dltBtn.textContent = "x";
   dltBtn.setAttribute("type", "button");
@@ -13,6 +17,64 @@ function subscribeNote(obj) {
     obj.remove();
   });
   noteDiv.appendChild(dltBtn);
+  
+
+  /*EDIT BUTTON*/
+  let editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.setAttribute("type", "button");
+
+  editBtn.addEventListener("click", () => {
+    let tableData = document.querySelectorAll(
+      `.display__container-note [data-id="${obj.id}"] .table__td`
+    );
+    tableData.forEach(function (td) {
+      let editInput;
+      if (td.classList.contains("table__td--title")) {
+        editInput = createBasicInput(
+          "table__edit-input",
+          "text",
+          "title",
+          "edit-title"
+        );
+        editInput.setAttribute("placeholder", obj.title);
+      } else if (td.classList.contains("table__td--details")) {
+        editInput = createTextArea(
+          "table__edit-input",
+          "details",
+          "edit-details"
+        );
+        editInput.textContent = obj.details;
+      } 
+      td.parentNode.replaceChild(editInput, td);
+    });
+
+    /* SUBMIT CHANGES BTN */
+    let submitChangeBtn = document.querySelector(".submit-edit-btn");
+    if (!submitChangeBtn) {
+      submitChangeBtn = document.createElement("button");
+      submitChangeBtn.textContent = "Submit";
+      submitChangeBtn.classList.add("submit-edit-btn");
+    }
+
+    submitChangeBtn.addEventListener("click", () => {
+      let editedInputs = document.querySelectorAll(" .table__edit-input");
+      editedInputs.forEach((input) => {
+        console.log(obj)
+        if (input.getAttribute("name") == "title") {
+          input.value == "" ?  obj.title : (obj.title = input.value);
+        } else if (input.getAttribute("name") == "details") {
+          console.log(input.value)
+          obj.details = input.value;
+        }
+      });
+      submitChangeBtn.remove();
+      displayMod.updateNotes();
+    });
+    noteDiv.appendChild(submitChangeBtn);
+  });
+
+  noteDiv.appendChild(editBtn);
   createTable(obj, noteDiv)
   noteDisplay.appendChild(noteDiv);
 }
