800004a469e304f66494ae0126e3aa3ac3578b6f
Bertmoss <b3rtm0ss@gmail.com>
2022-11-15 10:58:26 +0100

    Add alert to note

--- src/main/modal/__form/modal__form-note.js
+++ src/main/modal/__form/modal__form-note.js
@@ -4,6 +4,7 @@ import {
   createLabel,
 } from "../../../general/general__js/_input";
 import { NoteConstructor } from "../../main-pub-sub";
+import { createAlert } from "../../../general/general__js/_alert";
 
 const noteForm = document.createElement("form");
 noteForm.classList.add("form-note")
@@ -15,27 +16,35 @@ const noteTitle = createBasicInput(
   "note-title",
   "note-title"
 );
+noteTitle.setAttribute("required", "");
 const noteTitleLabel = createLabel(noteTitle, "Title:");
 appendLabelInput(noteForm, noteTitleLabel, noteTitle);
 
 const note = document.createElement("textarea");
 note.setAttribute("id", "note");
 note.setAttribute("name", "note");
+note.setAttribute("required", "");
 
 const noteLabel = createLabel(note, "Details:");
 appendLabelInput(noteForm, noteLabel, note);
 
 /* "submit" button */
 const submit = document.createElement("button");
-submit.setAttribute("type", "reset");
+submit.setAttribute("type", "button");
 submit.classList.add("form-note__btn")
 submit.textContent = "Create Note";
 noteForm.appendChild(submit);
 
+
+
 /* PUBSUB */
 
 function publishNote() {
+  if (noteTitle.value == "" || note.value == "") {
+    return createAlert("Please fill out both the title and details fields!")
+  } 
   let obj = new NoteConstructor(noteTitle.value, note.value);
+  noteForm.reset();
   obj.displayAll();
 }
 
